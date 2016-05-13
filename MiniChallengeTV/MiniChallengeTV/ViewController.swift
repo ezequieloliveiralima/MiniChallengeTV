589f396bc01dae27cb032b1b14df21e74ce33b15
ezequieloliveiralima <ezequieldeoliveiralima@gmail.com>
2016-05-13 17:24:55 -0300

    first commit

---
+++ MiniChallengeTV/MiniChallengeTV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiniChallengeTV
+//
+//  Created by Ezequiel de Oliveira Lima on 13/05/16.
+//  Copyright © 2016 BEPiD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
