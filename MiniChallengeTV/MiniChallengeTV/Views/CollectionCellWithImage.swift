5f4f766daf2623081a588fff3dd8faa362b3cf20
ezequieloliveiralima <ezequieldeoliveiralima@gmail.com>
2016-05-17 16:26:34 -0300

    Controllers, XIB's

---
+++ MiniChallengeTV/MiniChallengeTV/Views/CollectionCellWithImage.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionCellWithImage.swift
+//  MiniChallengeTV
+//
+//  Created by Ezequiel de Oliveira Lima on 17/05/16.
+//  Copyright © 2016 BEPiD. All rights reserved.
+//
+
+import UIKit
+
+class CollectionCellWithImage: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+}
