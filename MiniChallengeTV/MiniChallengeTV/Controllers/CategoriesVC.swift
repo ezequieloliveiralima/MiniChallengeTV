d1deb2da5c2e098b8a29baba527494c1d266f411
ezequieloliveiralima <ezequieldeoliveiralima@gmail.com>
2016-05-20 16:56:08 -0300

    Controllers

--- MiniChallengeTV/MiniChallengeTV/Controllers/CategoriesVC.swift
+++ MiniChallengeTV/MiniChallengeTV/Controllers/CategoriesVC.swift
@@ -11,13 +11,23 @@ import UIKit
 class CategoriesVC: UIViewController {
     @IBOutlet weak var collectionTop: UICollectionView!
     @IBOutlet weak var collectionAll: UICollectionView!
+    @IBOutlet weak var textSearch: UITextField!
 
-    var topCategories: [String]! = []
-    var allCategories: [String]! = []
+    var topCategories: List<Category>?
+    var allCategories: List<Category>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        MainController.getListTopCategories(SearchParameter.Page(1)) { (list) in
+            dispatch_async(dispatch_get_main_queue(), { 
+                self.topCategories = list
+                self.collectionTop.reloadData()
+            })
+        }
+        
+        let defaultCell = UINib(nibName: "DefaultCollectionCell", bundle: nil)
+        collectionTop.registerNib(defaultCell, forCellWithReuseIdentifier: "default-cell")
         // Do any additional setup after loading the view.
     }
 
@@ -36,4 +46,36 @@ class CategoriesVC: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+}
+
+extension CategoriesVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return (collectionView.tag == 0 ? topCategories?.list.count : allCategories?.list.count) ?? 0
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell: GenericCollectionCell!
+        
+        if collectionView.tag == 0 {
+            let category = topCategories?.list[indexPath.item]
+            cell = collectionView.dequeueReusableCellWithReuseIdentifier("default-cell",
+                                                                         forIndexPath: indexPath) as! GenericCollectionCell
+            cell.imageView.image = UIImage(named: "placeholder")
+        } else {
+            cell = collectionView.dequeueReusableCellWithReuseIdentifier("category",
+                                                                         forIndexPath: indexPath) as! GenericCollectionCell
+        }
+        
+        return cell
+    }
+}
+
+extension CategoriesVC: UITextFieldDelegate {
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        return true
+    }
 }
--- MiniChallengeTV/MiniChallengeTV/Controllers/HistoricVC.swift
+++ MiniChallengeTV/MiniChallengeTV/Controllers/HistoricVC.swift
@@ -10,20 +10,29 @@ import UIKit
 
 class HistoricVC: UITableViewController {
     
+    var historic: List<Product>?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.registerNib(UINib(nibName: "DefaultTableCell", bundle: nil), forCellReuseIdentifier: "default-cell")
     }
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 0
+        return 1
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return historic?.list.count ?? 0
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("")!
+        let cell = tableView.dequeueReusableCellWithIdentifier("default-cell") as! GenericTableCell
+        
+        let product = historic!.list[indexPath.row]
+        cell.label.text = "\(product.name)"
+//        cell.imgView.image = product.thumbnails?.first?.url
+        
         return cell
     }
 }
